4df766a03c234ff85807aa90902980f873c98665
Bryan Oltman <bryanoltman@gmail.com>
2023-07-20 10:03:58 -0400

    fix: verify min git version on Windows (#25)
    * fix: verify min git version on Windows
    
    * make Compare-SemVer more robust
    
    * Don't treat git version as semver

--- install.ps1
+++ install.ps1
@@ -3,8 +3,44 @@ $installDirectory = [IO.Path]::Combine($home, ".shorebird")
 function Test-GitInstalled {
     if (Get-Command git -ErrorAction SilentlyContinue) {
         Write-Debug "Git is installed."
-    } else {
+    }
+    else {
         Write-Output "No git installation detected. Git is required to use shorebird."
+        exit 1
+    }
+}
+
+function Compare-GitVersions {
+    param (
+        [string]$version1,
+        [string]$version2
+    )
+
+    $version1Components = $version1 -split '\.'
+    $version2Components = $version2 -split '\.'
+
+    for ($i = 0; $i -lt 3; $i++) {
+        $version1Number = [int]$version1Components[$i]
+        $version2Number = [int]$version2Components[$i]
+
+        if ($version1Number -lt $version2Number) {
+            return -1
+        }
+        elseif ($version1Number -gt $version2Number) {
+            return 1
+        }
+    }
+
+    return 0
+}
+
+function Test-GitVersion {
+    $minGitVersion = "2.37.1"
+    $gitVersion = (Get-Command git).FileVersionInfo.ProductVersion
+
+    $comparisonResult = Compare-GitVersions -version1 $gitVersion -version2 $minGitVersion
+    if ($comparisonResult -eq -1) {
+        Write-Output "Installed version $gitVersion is older than required version $minGitVersion."
         exit 1
     }
 }
@@ -23,6 +59,7 @@ function Update-Path {
 }
 
 Test-GitInstalled
+Test-GitVersion
 
 $force = $args -contains "--force"
 
@@ -30,7 +67,8 @@ if (Test-Path $installDirectory) {
     if ($force) {
         Write-Output "Existing Shorebird installation detected. Overwriting..."
         Remove-Item -Recurse -Force $installDirectory
-    } else {
+    }
+    else {
         Write-Output "Error: Existing Shorebird installation detected. Use --force to overwrite."
         return
     }
@@ -46,7 +84,7 @@ Pop-Location
 
 $wasPathUpdated = Update-Path
 # 1F426 is the code for 🐦. See https://unicode.org/emoji/charts/full-emoji-list.html#1f426.
-$birdEmoji = [System.Char]::ConvertFromUtf32([System.Convert]::toInt32("1F426",16))
+$birdEmoji = [System.Char]::ConvertFromUtf32([System.Convert]::toInt32("1F426", 16))
 
 Write-Output @"
 
